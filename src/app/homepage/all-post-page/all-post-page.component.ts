51f40ca1d6525045ce20814e652a763b2bb8fd11
mikeh100 <mikehellenbrand@hotmail.com>
2020-05-04 12:29:57 +0900

    showing all posts to the all post page

--- src/app/homepage/all-post-page/all-post-page.component.ts
+++ src/app/homepage/all-post-page/all-post-page.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AllPostsService } from './service/all-posts.service'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-all-post-page',
   templateUrl: './all-post-page.component.html',
   styleUrls: ['./all-post-page.component.scss']
 })
-export class AllPostPageComponent implements OnInit {
+export class AllPostPageComponent implements OnInit, OnDestroy {
   public allPosts: any;
+  private getAllPostSubscrition: Subscription;
+  private getUserName: Subscription;
 
   constructor(
     public allPostService: AllPostsService
   ) { }
 
   ngOnInit(): void {
-    this.allPostService.getAllPosts().subscribe(data => {
+    this.getAllPostSubscrition = this.allPostService.getAllPosts().subscribe(data => {
+      data.forEach(value => {
+        this.getUserName = this.allPostService.getUserName(value.uid).subscribe(userName => {
+          value.userName = userName[0].name;
+        });
+      });
       this.allPosts = data;
     });
   }
 
+  ngOnDestroy() {
+    this.getAllPostSubscrition.unsubscribe();
+    this.getUserName.unsubscribe();
+  }
 }
